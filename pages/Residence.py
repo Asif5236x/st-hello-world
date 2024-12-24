d0ecc4183cb1099bfe7a36ec49cab4bd8f0503a1
Asif5236x <asif5236x@gmail.com>
2024-12-24 21:47:13 +0600

    Update Residence.py
    ref-Ref

--- pages/Residence.py
+++ pages/Residence.py
@@ -1,10 +1,8 @@
 import streamlit as st
 from PIL import Image
 
-
 # Cover Image and Description
-cover_image=r'ref/Residence.jpg'
-
+cover_image = r'Ref/Residence.jpg'
 
 col1, col2 = st.columns(2)
 with col1:
@@ -16,25 +14,22 @@ with col2:
     st.write('Residences are essential for family togetherness and bonding. A key element of traditional Bangladeshi homes is the উঠান, which promotes this connection. The courtyard and landscaped areas provide a breathable environment, enhancing the quality of life.')
 st.write("___")
 
-
-
 # Form Generation
 st.markdown('<p style="color: Grey; font-size: 30px; font-weight: bold;">Form Generation</p>', unsafe_allow_html=True)
-Form_gen=r'ref/Form gen Residence.jpg'
+Form_gen = r'Ref/Form gen Residence.jpg'
 st.image(Form_gen)
 st.write("This residence features a central courtyard design, promoting family togetherness and enhancing natural ventilation. The open, communal space fosters connection among family members while providing a peaceful retreat, ideal for Bangladesh’s climate and lifestyle.")
 st.write("___")
 st.header("")
 
-
 # Floor Plan
 st.markdown('<p style="color: Grey; font-size: 30px; font-weight: bold;">Floor Plan</p>', unsafe_allow_html=True)
 st.header("")
-Ground_floor=r'ref/Ground Floor Residence.jpg'
+Ground_floor = r'Ref/Ground Floor Residence.jpg'
 st.image(Ground_floor)
 st.header("")
 st.header("")
-First_floor=r'ref/First Floor Residence.jpg'
+First_floor = r'Ref/First Floor Residence.jpg'
 st.image(First_floor)
 st.write("___")
 st.write("")
@@ -42,34 +37,31 @@ st.write("")
 # Exploded
 st.markdown('<p style="color: Grey; font-size: 30px; font-weight: bold;">Exploded</p>', unsafe_allow_html=True)
 
-Exploded=r'ref/Exploded.jpg'
+Exploded = r'Ref/Exploded.jpg'
 st.image(Exploded)
 st.write("___")
 
-
 # Visualizations
-
 st.markdown('<p style="color: Grey; font-size: 30px; font-weight: bold;">Visualizations</p>', unsafe_allow_html=True)
 st.write("")
-Axono=r'ref/Axono.jpg'
-Ele1=r'ref/Ele1.jpg'
-Ele2=r'ref/Ele2.jpg'
+Axono = r'Ref/Axono.jpg'
+Ele1 = r'Ref/Ele1.jpg'
+Ele2 = r'Ref/Ele2.jpg'
 col1, col2 = st.columns(2)
 with col1:
     st.image(Axono)
     st.write("Birds Eye View")
 with col2:
     st.image(Ele1, width=280)
-    st.image(Ele2,width=280)
+    st.image(Ele2, width=280)
     st.write("Elevations")
 st.write("___")
-
 
 # Sections
 st.markdown('<p style="color: Grey; font-size: 30px; font-weight: bold;">Sections</p>', unsafe_allow_html=True)
 
-Section_Residence=r'ref/Section Residence.jpg'
-Section_with_Blowup=r'ref/Section with blowup.jpg'
+Section_Residence = r'Ref/Section Residence.jpg'
+Section_with_Blowup = r'Ref/Section with blowup.jpg'
 st.image(Section_Residence)
 st.write("Section AA")
 st.write("___")
@@ -77,10 +69,3 @@ st.image(Section_with_Blowup)
 st.write("Section BB")
 st.header("")
 st.write("___")
-
-
-
-
-
-
-
